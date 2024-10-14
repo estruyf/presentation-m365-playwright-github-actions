af21fa3d3e6c4d2ce55d12b3edb1e1640b8c6084
Elio Struyf <elio@struyfconsulting.be>
2024-10-14 15:02:51 +0200

    Reporting

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -76,6 +76,23 @@ if (config.reporter && config.reporter instanceof Array) {
         azureStorageUrl: process.env.AZURE_STORAGE_URL,
       },
     ]);
+
+    let runUrl = "";
+    if (process.env.GITHUB_RUN_ID) {
+      const runId = process.env.GITHUB_RUN_ID;
+      const repo = process.env.GITHUB_REPOSITORY;
+      runUrl = `${process.env.GITHUB_SERVER_URL}/${repo}/actions/runs/${runId}`;
+    }
+
+    config.reporter.push([
+      "playwright-msteams-reporter",
+      <MsTeamsReporterOptions>{
+        webhookUrl: process.env.M365_WEBHOOK_URL,
+        webhookType: "powerautomate",
+        linkToResultsUrl: runUrl,
+        mentionOnFailure: process.env.M365_USERNAME,
+      },
+    ]);
   } else {
     config.reporter.push(["html"]);
   }

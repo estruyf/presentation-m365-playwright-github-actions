f27af81cfec72b2405a642979288ef7d8633ad11
Elio Struyf <elio@struyfconsulting.be>
2024-10-17 12:02:31 +0200

    add artifacts URL

--- .demo/4.reporting/step1.ts
+++ .demo/4.reporting/step1.ts
@@ -3,6 +3,7 @@
       "@estruyf/github-actions-reporter",
       {
         showError: true,
+        showArtifactsLink: true,
         azureStorageSAS: process.env.AZURE_STORAGE_SAS,
         azureStorageUrl: process.env.AZURE_STORAGE_URL,
       },

d36a7de091d4e280022b40615c62a0dc2a250e13
Elio Struyf <elio@struyfconsulting.be>
2025-05-28 16:34:24 +0200

    Added artifacts

--- e2e/tests/login.setup.ts
+++ e2e/tests/login.setup.ts
@@ -1,29 +1,52 @@
 import { test as setup } from "@playwright/test";
+import * as OTPAuth from "otpauth";
 import { existsSync } from "fs";
 
-const authFile = "playwright/.auth/user.json";
-
-// More info: https://playwright.dev/docs/auth
+const AuthFile = "playwright/.auth/user.json";
 
 setup("authenticate", async ({ page }) => {
-  // Check if the auth file exists
-  if (existsSync(authFile)) {
+  if (existsSync(AuthFile)) {
     return;
   }
 
   await page.goto(process.env.M365_PAGE_URL || "");
 
-  page.locator("input[type=email]").fill(process.env.M365_USERNAME || "");
-
+  const emailInput = page.locator("input[type=email]");
+  await emailInput.click();
+  await emailInput.fill(process.env.M365_USERNAME || "");
   await page.getByRole("button", { name: "Next" }).click();
 
-  page.locator("input[type=password]").fill(process.env.M365_PASSWORD || "");
+  const passwordInput = page.locator("input[type=password]");
+  await passwordInput.click();
+  await passwordInput.fill(process.env.M365_PASSWORD || "");
+  await page.locator("input[type=submit]").click();
 
-  await Promise.all([
-    await page.locator("input[type=submit][value='Sign in']").click(),
-    await page.locator("input[type=submit][value='Yes']").click(),
-    await page.waitForURL(process.env.M365_PAGE_URL || ""),
-  ]);
+  // Check if there is a Microsft Authenticator app prompt
+  const otherWayLink = page.locator("a#signInAnotherWay");
+  await otherWayLink.waitFor({ timeout: 5000 });
+  if (await otherWayLink.isVisible()) {
+    await otherWayLink.click();
 
-  await page.context().storageState({ path: authFile });
+    const otpLink = page.locator(`div[data-value="PhoneAppOTP"]`);
+    await otpLink.click();
+  }
+
+  // Fill in the OTP code
+  const otpInput = await page.waitForSelector("input#idTxtBx_SAOTCC_OTC");
+  let totp = new OTPAuth.TOTP({
+    issuer: "Microsoft",
+    label: process.env.M365_USERNAME,
+    algorithm: "SHA1",
+    digits: 6,
+    period: 30,
+    secret: process.env.M365_OTP_SECRET,
+  });
+  const code = totp.generate();
+  await otpInput.fill(code);
+
+  await page.locator("input[type=submit]").click();
+  await page.locator("input[type=submit][value='Yes']").click();
+  await page.waitForURL(process.env.M365_PAGE_URL || "");
+
+  await page.context().storageState({ path: AuthFile });
 });
